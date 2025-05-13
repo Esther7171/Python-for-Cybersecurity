14d6d294525d7227a3469b67ac359c89cd10f9ba
deathesther <122229257+Esther7171@users.noreply.github.com>
2025-05-14 00:38:34 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,81 @@
+// Testing Print
+// class demo{
+//     public static void main(String args []){
+//         System.out.print("Hello World");
+//         System.out.print("Hello World");
+//     }
+// }
+
+
+// Testing Println
+// class demo{
+//     public static void main(String args []){
+//         System.out.println("Hello World");
+//         System.out.print("Hello World");
+//     }
+// }
+
+
+// Testing \n
+// class demo{
+//     public static void main(String args []){
+//         System.out.print("Hello World\n");
+//         System.out.print("Hello World\n");
+//         System.out.print("Hello World");
+//     }
+// }
+
+
+// Testing \n
+// class demo{
+//     public static void main(String args []){
+//         System.out.print("Hello World\nTesting This");
+//     }
+// }
+
+
+// // Question
+// class demo{
+//     public static void main(String args[]){
+//         // with print + println
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//         System.out.println("*****");
+//         System.out.println("");
+//         // with print + \n
+//         System.out.print("*\n**\n***\n****\n*****");
+//     }
+// }
+
+
+// // Testing variable
+// class demo{
+//     public static void main(String args[]){
+//        String name = "Death";
+//        int a = 2;
+//        int b = 4;
+//        double price = 25.25;
+//        System.out.println(name);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(price);
+//     }
+// }
+
+
+// Changing Variable values
+// class demo{
+//     public static void main(String args[]){
+//        String name = "Death";
+//        int a = 2;
+//        int b = 4;
+//        double price = 25.25;  
+//        a = 10;
+//        name = "Esther";
+//        System.out.println(name);
+//        System.out.println(a);
+//        System.out.println(b);
+//     }
+// }
