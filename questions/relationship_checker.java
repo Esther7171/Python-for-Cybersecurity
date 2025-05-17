169b177e5f52fd30896a073e7f8003ac819bc4f0
deathesther <122229257+Esther7171@users.noreply.github.com>
2025-05-17 15:13:15 +0530

    Create relationship_checker.java

---
+++ questions/relationship_checker.java
@@ -0,0 +1,34 @@
+package questions;
+//  Write a program that checks the relationship between two variables a and b. If a == b, print 'equal'; if a > b, print 'a is greater'; and if a < b, print 'a is lesser'
+
+import java.util.*;
+
+public class relationship_checker {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // for a 
+        System.out.print("Enter the 1st number = ");
+        int a = sc.nextInt();
+        // for b
+        System.out.print("Enter the 2nd number = ");
+        int b = sc.nextInt();
+
+        // condition start here:
+
+        if(a == b){
+            System.out.println("Equal");
+        }
+        else if (a > b){
+                System.out.println("a is greater");
+            }
+        else{
+                System.out.println("a is lesser");
+            }
+        
+        }
+
+                
+}
+
+ 
